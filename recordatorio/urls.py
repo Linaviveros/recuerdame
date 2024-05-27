e0a34084134d50bedc7aded4c897a4c29efee0a4
lina viveros <lina.viverosm@campusucc.edu.co>
2024-05-27 18:48:53 -0500

    rutasN

--- recordatorio/urls.py
+++ recordatorio/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_recordatorios, name='lista_recordatorios'),
+    path('crear/', views.crear_recordatorio, name='crear_recordatorio'),
+    path('actualizaciones/', views.obtener_actualizaciones, name='obtener_actualizaciones'),
 ]
+
