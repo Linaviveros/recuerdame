46786f84d97082a133470f4a2c792cf42d706f12
lina viveros <lina.viverosm@campusucc.edu.co>
2024-05-27 18:14:19 -0500

    modelo view

--- recordatorio/models.py
+++ recordatorio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Recordatorio(models.Model):
+    nombre_medicamento = models.CharField(max_length=200)
+    hora_alerta = models.TimeField()
+    comentario = models.TextField()
+
+    def __str__(self):
+        return self.nombre_medicamento
--- recordatorio/views.py
+++ recordatorio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def lista_recordatorios(request):
+    recordatorios = Recordatorio.objects.all()
+    return render(request, 'recordatorios/lista_recordatorios.html', {'recordatorios': recordatorios})
